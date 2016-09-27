44a8992605662f24e95d093153f24511019c36d6
Daniel Lim <hbt@markhamboards.com>
2016-09-26 22:57:42 -0400

    Try again
    Try again

--- project-6.js
+++ project-6.js
@@ -3,17 +3,17 @@ const dbName = process.argv[2];
 const Url = 'mongodb://localhost:27014/' + dbName;
 const users = db.collection('users');
 
-mongo.connect(Url).then(db => {
-  db.collection('users', (err, col) => {
-    const query = { username: 'tinatime' }
-    const changeAge = { $set: { age: 40 } }
-    users.update(query, changeAge, (err, res) => {
-      if (err) console.error(err);
-      else console.log('updated');
-      db.close()
-    })
-  })
-  .catch(err => {
-    console.error(err);
-  })
+mongo.connect(Url, (err, db) {
+
+   users.update({
+			username: 'tinatime'
+		}, {
+			$set: {
+				age: 40
+			}
+		}, function(err) {
+			if (err) throw err;
+			db.close();
+		})
+
 })
