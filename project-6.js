9f1fc181b83996364f8c92e4e5f7a4afa13afe6e
Daniel Lim <hbt@markhamboards.com>
2016-09-26 22:06:49 -0400

    try pro 6 again
    try pro 6 again

--- project-6.js
+++ project-6.js
@@ -2,3 +2,20 @@ var mongo = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/learnyoumongo";
 
 mongo.connect(url, function(err, db) {
+
+    if (err) return console.error(err);
+
+    var col = db.collection(process.argv[2]);
+
+    col.remove({
+
+        _id: process.argv[3]
+
+    }, function(err) {
+
+        if (err) throw err;
+        
+        db.close();
+
+    });
+});
