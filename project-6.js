f3c557d78f8d4d3176e3d0e92afdb954c380b2bd
Daniel Lim <hbt@markhamboards.com>
2016-09-26 22:54:44 -0400

    what am i missing
    missing?

--- project-6.js
+++ project-6.js
@@ -1,21 +1,19 @@
-const mongo = require('mongodb').MongoClient
-const dbName = process.argv[2]
-const Url = 'mongodb://localhost:27017/' + dbName
+const mongo = require('mongodb').MongoClient;
+const dbName = process.argv[2];
+const Url = 'mongodb://localhost:27014/' + dbName;
+const users = db.collection('users');
 
-
-    mongo.connect(Url).then(db => {
-
-        db.collection('users', (err, col) => {
-
-            const query = { username: 'tinatime' }
-
-            const delta = { $set: { age: 40 } }
-
-          col.update(query, delta, (err, res) => {
-               if (err) console.error(err);
-               else console.log('updated');
-               db.close();
-           });
-       });
-       .catch(err => { console.error(err) })
-   })
+mongo.connect(Url).then(db => {
+  db.collection('users', (err, col) => {
+    const query = { username: 'tinatime' }
+    const changeAge = { $set: { age: 40 } }
+    users.update(query, changeAge, (err, res) => {
+      if (err) console.error(err);
+      else console.log('updated');
+      db.close()
+    })
+  })
+  .catch(err => {
+    console.error(err);
+  })
+})
