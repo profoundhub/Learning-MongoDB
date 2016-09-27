9bd8f9478a977b1537c1dc3361e436bebdb48192
Daniel Lim <hbt@markhamboards.com>
2016-09-27 14:47:42 -0400

    define
    define

--- Project-8.js
+++ Project-8.js
@@ -1,9 +1,28 @@
-var mongo = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/' + process.argv[2];
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/learnyoumongo';
+const age = process.argv[2];
 
+    mongo.connect(url, function(err, db) {
+    	if (err) throw err;
 
- collection.count({
-      name: 'foo'
-    }, function(err, count) {
-
+    	db.collection('parrots').count({
+    		age: {
+    			$gt: +age
+    		}
+    	}, function(err, count) {
+    		console.log(count);
+    		db.close();
+    	})
     })
+
+/*
+    db.runCommand(
+        {   count:'orders',
+            query: { ord_dt:
+                {
+                    $gt: new Date('01/01/2012')
+                }
+            }
+        }
+    )
+*/
