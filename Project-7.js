ecce60031cde67a43bedacfce120d6059133c9b4
Daniel Lim <hbt@markhamboards.com>
2016-09-27 13:51:07 -0400

    set project 7 js
    set project 7 js

---
+++ Project-7.js
@@ -0,0 +1,17 @@
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/' + process.argv[2];
+
+mongo.connect(url, function(err, db) {
+
+    var collection = db.collection(process.argv[3]);
+	var _id = process.argv[4];
+
+		collection.remove({
+			_id: _id
+		},
+		function(err) {
+		    if (err) throw err;
+			db.close();
+		});
+
+});
