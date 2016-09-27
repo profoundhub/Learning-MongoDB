9c0178bb6fa4cbc33ab4f0c6a699eb5033a621c3
Daniel Lim <hbt@markhamboards.com>
2016-09-26 22:45:07 -0400

    messy
    currently messy

--- project-6.js
+++ project-6.js
@@ -3,19 +3,19 @@ const dbName = process.argv[2]
 const Url = 'mongodb://localhost:27017/' + dbName
 
 
-mongo.connect(Url).then(db => {
+    mongo.connect(Url).then(db => {
 
-      db.collection('users', (err, col) => {
+        db.collection('users', (err, col) => {
 
-          const query = { username: 'tinatime' }
+            const query = { username: 'tinatime' }
 
-          const delta = { $set: { age: 40 } }
+            const delta = { $set: { age: 40 } }
 
-})
-
-...
-
-        db.close();
-
-    });
-});
+          col.update(query, delta, (err, res) => {
+               if (err) console.error(err);
+               else console.log('updated');
+               db.close();
+           });
+       });
+       .catch(err => { console.error(err) })
+   })
